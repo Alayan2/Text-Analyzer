d95706232831f642c39c40e80328b9e09e3133b4
Alayan2 <90277439+Alayan2@users.noreply.github.com>
2021-09-16 13:09:48 -0400

    Text Analyzer Version 1
    - Read file and convert to String
    - Use regex to clean up string 
    - Store data as array
    - Loop through array, adding to count Integer variable for each recurrence
    - Once looped through the entire String array, add count variable and word to ArrayList as an object.
    - Sort ArrayList using Comparator
    - Print top 20 word frequency pairings
    - Print all word frequency pairings

---
+++ TextAnalyzer/src/WordCount.java
@@ -0,0 +1,106 @@
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 
+ */
+
+/**
+ * Assignment Description
+ * Write a text analyzer that reads a file and outputs statistics 
+ * about that file. It should output the word frequencies of all 
+ * words in the file, sorted by the most frequently used word. 
+ * The output should be a set of pairs, each pair containing a 
+ * word and how many times it occurred in the file.
+ * 
+ * @author alayan2
+ *
+ */
+public class WordCount {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// read whole file as String 
+		String data = "";
+		try {
+			data = new String(Files.readAllBytes(Paths.get("1065-h.htm")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String[] textArray = clean(data);
+
+		ArrayList<WordFrequencyPairing> textAnalyzer = new ArrayList<WordFrequencyPairing>();
+
+		Integer count = 0;
+		for (int i = 0; i < textArray.length; i++) {
+			if(!(textAnalyzer.toString().contains(textArray[i] + " "))) {
+				for (int j = 1; j < textArray.length; j++) {
+					if(textArray[i].equals(textArray[j])) {
+						count++;
+					}
+				}
+			}
+			String newText = textArray[i].toString();
+			if(!(textAnalyzer.toString().contains(count + ": " + textArray[i]))) {
+				textAnalyzer.add(new WordFrequencyPairing(count, newText));
+			}
+			count = 0;
+		}
+
+		Comparator<WordFrequencyPairing> countComparator = (c1, c2) -> (int) (c1.getCount() - c2.getCount());
+
+		textAnalyzer.sort(Collections.reverseOrder(countComparator));	    
+		
+	    //Sublist to List
+	     List<WordFrequencyPairing> list = textAnalyzer.subList(0, 20);
+	     System.out.println("Top 20 Most Frequently Used Words in 'The Raven': "+list);
+	     System.out.println("\n" + "All Word Frequencies in 'The Raven': "+textAnalyzer.toString());
+
+	}
+
+
+	/**Return an array of the words in a text file 
+	 * removes all html tags, punctuation and split by spaces
+	 * @param textFile, file of text separated by whitespace
+	 * @return array of words in textFile
+	 */
+	public static String[] clean(String textFile){
+		if(textFile == null || textFile.isEmpty()){
+			return null;
+		}
+
+		//removes text above poem title
+		String textFile2 = textFile.substring(textFile.indexOf("/big>"));
+
+		//removes text after poem
+		String textFile3 = textFile2.substring(0, textFile2.indexOf("***"));
+
+		//removes all html tags between '< >'
+		textFile3 = textFile3.replaceAll("\\<.*?\\>", "" );
+
+		//removes dashes
+		String textFile4 = textFile3.replaceAll("&mdash", " ");
+
+		//removes all remaining punctuation
+		String textFile5 = textFile4.replaceAll("\\p{Punct}", " ");
+
+		//removes all remaining punctuation, converts to all lower case letters, and splits string for array
+		String[] words = textFile4.replaceAll("[^a-zA-Z ]", " ").toLowerCase().split("\\s+");
+
+		return words;
+	}
+}
---
+++ TextAnalyzer/src/WordFrequencyPairing.java
@@ -0,0 +1,36 @@
+
+public class WordFrequencyPairing {
+	private Integer count;
+	private String word;
+	
+	public WordFrequencyPairing(Integer count, String word) {
+	
+		this.count = count;
+		this.word = word;
+
+	}
+	  
+	// Getter
+	  public Integer getCount() {
+	    return count;
+	  }
+	  
+	  public String getWord() {
+		    return word;
+		  }
+
+	  // Setter
+	  public void setCount(Integer newCount) {
+	    this.count = newCount;
+	  }
+
+	  public void setWord(String newWord) {
+		    this.word = newWord;
+		  }
+	  
+	  @Override
+	    public String toString(){
+	        return  "\n" + count + ": " + word ; 
+	    }
+}
+
